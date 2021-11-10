ab44f3a279fbe495bbbbe6188b0c1eba1cf93e7a
tommacaluso1 <tommcalusolavoro@hotmail.com>
2021-11-10 14:21:53 -0300

    postcss-loader added

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,12 @@
 const tailwindcss = require('tailwindcss');
 module.exports = {
   plugins: [
-    'postcss-preset-env',
-    tailwindcss
+    [
+      "postcss-preset-env",
+      tailwindcss,
+      {
+        // Options
+      },
+    ],
   ],
 };
